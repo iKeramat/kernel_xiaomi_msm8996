dcd393ce35c43dcd5de1b22e6650872d4dcec0b8
JonasCardoso <jonascard60@gmail.com>
2018-08-27 12:02:31 -0300

    drivers: soc: msm_performance: disabled touchboost

--- drivers/soc/qcom/msm_performance.c
+++ drivers/soc/qcom/msm_performance.c
@@ -30,7 +30,7 @@
 #ifndef CONFIG_MSM_PERFORMANCE_CPUFREQ_LIMITS_VOTING_ONLY
 static unsigned int use_input_evts_with_hi_slvt_detect;
 static struct mutex managed_cpus_lock;
-static int touchboost = 1;
+static int touchboost = 0;
 
 /* Maximum number to clusters that this module will manage*/
 static unsigned int num_clusters;
